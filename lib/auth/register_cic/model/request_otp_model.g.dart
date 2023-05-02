3ee9f617845c09140bef4b5dbc40dce32fc7ce28
Tinut-Chan <tinutchan@gmail.com>
2023-05-02 11:50:00 +0700

    ss

--- lib/auth/register_cic/model/request_otp_model.g.dart
+++ lib/auth/register_cic/model/request_otp_model.g.dart
@@ -1,7 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: non_constant_identifier_names
-
 part of 'request_otp_model.dart';
 
 // **************************************************************************
--- lib/modules/ut_trading/screens/ut_trading_screen.dart
+++ lib/modules/ut_trading/screens/ut_trading_screen.dart
@@ -1,12 +1,72 @@
+import 'package:cic_mobile/constants/color_app/color_app.dart';
+import 'package:cic_mobile/widgets/custom_appbar_blue_bg.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get_utils/get_utils.dart';
 
 class UTTradingScreen extends StatelessWidget {
   const UTTradingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('UT Trading')),
+    return CupertinoPageScaffold(
+      child: Scaffold(
+        backgroundColor: const Color(0xffE5E5E5),
+        body: Stack(
+          children: [
+            SizedBox(
+              width: context.width,
+              height: context.height,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: SvgPicture.asset(
+                  'assets/icons/ut_trading/background_appbar.svg',
+                  width: context.width,
+                ),
+              ),
+            ),
+            Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: customAppBar(
+                backgroundColor: Colors.transparent,
+                context: context,
+                title: 'UT Trading',
+                centerTitle: false,
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: SvgPicture.asset(
+                      'assets/icons/ut_trading/help.svg',
+                    ),
+                  ),
+                ],
+              ),
+              body: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: AppColor.mainColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [SvgPicture.asset('assetName')],
+                        ),
+                        Text('2'),
+                        Text('3'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
